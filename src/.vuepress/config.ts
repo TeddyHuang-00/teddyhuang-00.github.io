d54561e527d848c926af3fd5e619c7a217cafa72
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2024-01-26 10:51:58 -0500

    fix: :wrench: Remove docsearchPlugin and update shikiPlugin configuration

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,7 @@
-import { defineUserConfig, viteBundler } from "vuepress";
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
 import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
 import { getDirname, path } from "@vuepress/utils";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import { shikiPlugin } from "@vuepress/plugin-shiki";
 import { redirectPlugin } from "vuepress-plugin-redirect";
 import tailwindcss from "tailwindcss";
@@ -44,107 +44,34 @@ export default defineUserConfig({
   theme,
 
   plugins: [
-    registerComponentsPlugin({
-      componentsDir: path.resolve(__dirname, "./components"),
-    }),
     shikiPlugin({
       theme: "dark-plus",
+      langs: [
+        "ts",
+        "py",
+        "yaml",
+        "r",
+        "cpp",
+        "rust",
+        "csharp",
+        "json",
+        "makefile",
+        "ps",
+        "vue",
+        "vue-html",
+        "md",
+        "bash",
+        "diff",
+      ],
+    }),
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, "./components"),
     }),
     redirectPlugin({
       autoLocale: true,
       localeConfig: {
         "/zh/": ["zh-CN", "zh-TW", "zh"],
         "/en/": ["en-US", "en-UK", "en"],
-      },
-    }),
-    docsearchPlugin({
-      appId: "GQWRTHZR5O",
-      apiKey: "29d33a940d6df255b8e18ce923022671",
-      indexName: "teddyhuang-00io",
-      locales: {
-        "/zh/": {
-          placeholder: "搜索站点",
-          translations: {
-            button: {
-              buttonText: "搜索站点",
-              buttonAriaLabel: "搜索站点",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-                reportMissingResultsText: "你认为该查询应该有结果？",
-                reportMissingResultsLinkText: "点击反馈",
-              },
-            },
-          },
-        },
-        "/en/": {
-          placeholder: "Search site",
-          translations: {
-            button: {
-              buttonText: "Search site",
-              buttonAriaLabel: "Search site",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "Reset query",
-                resetButtonAriaLabel: "Reset query",
-                cancelButtonText: "Cancel",
-                cancelButtonAriaLabel: "Cancel",
-              },
-              startScreen: {
-                recentSearchesTitle: "Recent searches",
-                noRecentSearchesText: "No recent searches",
-                saveRecentSearchButtonTitle: "Save to recent searches",
-                removeRecentSearchButtonTitle: "Remove from recent searches",
-                favoriteSearchesTitle: "Favorites",
-                removeFavoriteSearchButtonTitle: "Remove from favorites",
-              },
-              errorScreen: {
-                titleText: "Unable to get results",
-                helpText: "You might need to check your network connection",
-              },
-              footer: {
-                selectText: "Select",
-                navigateText: "Navigate",
-                closeText: "Close",
-                searchByText: "Search by",
-              },
-              noResultsScreen: {
-                noResultsText: "No results found",
-                suggestedQueryText: "You can try searching for",
-                reportMissingResultsText:
-                  "Do you think this query should have results?",
-                reportMissingResultsLinkText: "Click here to report it",
-              },
-            },
-          },
-        },
       },
     }),
   ],
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -155,6 +155,97 @@ export default hopeTheme({
       license: "CC BY-NC-SA 4.0",
     },
 
+    docsearch: {
+      appId: "GQWRTHZR5O",
+      apiKey: "29d33a940d6df255b8e18ce923022671",
+      indexName: "teddyhuang-00io",
+      locales: {
+        "/zh/": {
+          placeholder: "搜索站点",
+          translations: {
+            button: {
+              buttonText: "搜索站点",
+              buttonAriaLabel: "搜索站点",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+        "/en/": {
+          placeholder: "Search site",
+          translations: {
+            button: {
+              buttonText: "Search site",
+              buttonAriaLabel: "Search site",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "Reset query",
+                resetButtonAriaLabel: "Reset query",
+                cancelButtonText: "Cancel",
+                cancelButtonAriaLabel: "Cancel",
+              },
+              startScreen: {
+                recentSearchesTitle: "Recent searches",
+                noRecentSearchesText: "No recent searches",
+                saveRecentSearchButtonTitle: "Save to recent searches",
+                removeRecentSearchButtonTitle: "Remove from recent searches",
+                favoriteSearchesTitle: "Favorites",
+                removeFavoriteSearchButtonTitle: "Remove from favorites",
+              },
+              errorScreen: {
+                titleText: "Unable to get results",
+                helpText: "You might need to check your network connection",
+              },
+              footer: {
+                selectText: "Select",
+                navigateText: "Navigate",
+                closeText: "Close",
+                searchByText: "Search by",
+              },
+              noResultsScreen: {
+                noResultsText: "No results found",
+                suggestedQueryText: "You can try searching for",
+                reportMissingResultsText:
+                  "Do you think this query should have results?",
+                reportMissingResultsLinkText: "Click here to report it",
+              },
+            },
+          },
+        },
+      },
+    },
+
     git: {
       // 展示贡献者
       contributors: false,
