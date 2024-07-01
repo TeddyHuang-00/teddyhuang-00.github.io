b5285cdc84aadcf30491720c45f19fc0c564668a
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2024-07-01 11:04:02 +0800

    feat: Add Rubby component for displaying ruby annotations

---
+++ src/.vuepress/components/Rubby.vue
@@ -0,0 +1,24 @@
+<template>
+  <ruby>
+    <slot></slot>
+    <rp>(</rp>
+    <rt>{{ props.anno }}</rt>
+    <rp>)</rp>
+  </ruby>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  anno: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style>
+ruby > rt {
+  font-size: 100%;
+  zoom: 0.75;
+}
+</style>
