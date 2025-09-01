a87312527a6b5c55282f1e05ca989c5b3a524ea4
Nan Huang <teddyhuangnan@gmail.com>
2025-09-01 15:02:34 -0400

    Add Ruby component for easier creation of ruby text in articles.

--- src/components/content/components.ts
+++ src/components/content/components.ts
@@ -1,7 +1,8 @@
 import Aside from "./Aside.astro";
 import Badge from "./Badge.astro";
+import Ruby from "./Ruby.astro";
 import Steps from "./Steps.astro";
 import TabItem from "./TabItem.astro";
 import Tabs from "./Tabs.astro";
 
-export { Aside, Badge, Steps, TabItem, Tabs };
+export { Aside, Badge, Ruby, Steps, TabItem, Tabs };
